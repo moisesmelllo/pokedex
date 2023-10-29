1c1bfbecfdeccb6b82be0589915d38d373020c65
Moises Melo Viana Ferreira <moisesmvferreira@gmail.com>
2023-10-29 17:51:38 -0300

    default link to home page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +12,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Link href='/'>
+          <h1 className='text-4xl text-white text-bold text-center'>Pokedex</h1>
+        </Link>
+        {children}
+      </body>
     </html>
   )
 }
